1c1a0f55a92822c4a13eaa597a38ed86ddbf8be4
Nikita Shpanko <shpancore@gmail.com>
2021-08-07 17:05:02 +0300

    added sound

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import lipsPNG from './assets/lips.png';
 import lipsJSON from './assets/lips.json';
 import facePNG from './assets/KeyFace 12.png';
 import eyesPNG from './assets/KeyFace pupils.png';
+import vowelsMP3 from './assets/vowels.mp3';
+import vowelsOGG from './assets/vowels.ogg';
 
 import config from './param/config.json';
 import headParts from './param/headParts.json';
 import lipAnim from './param/lipAnim.json';
 import behavior from './param/behavior.json';
+import vowelMarkers from './param/vowels.json';
 
 const MAX_DIST = Phaser.Math.Distance.Between(config.width, config.height, 0, 0) / 2;
 
@@ -17,11 +20,13 @@ class MyGame extends Phaser.Scene {
     super();
   }
   preload() {
+    this.load.audio('vowels', [vowelsMP3, vowelsOGG]);
     this.load.atlas('lips', lipsPNG, lipsJSON);
     this.load.image('face', facePNG);
     this.load.image('eyes', eyesPNG);
   }
   create() {
+    const vowels = this.sound.add('vowels');
     const face = this.add.image(0, 0, 'face');
     const eyes = this.add.image(0, headParts.eyes.defaultY, 'eyes');
     const eyeContainer = this.add.container(0, headParts.eyes.lineY, eyes);
@@ -49,11 +54,19 @@ class MyGame extends Phaser.Scene {
           align: 'left',
         })
         .setInteractive();
+      vowels.addMarker({
+        name: key,
+        start: vowelMarkers[key].start,
+        duration: vowelMarkers[key].end - vowelMarkers[key].start,
+        config: { delay: behavior.soundDelay },
+      });
     });
 
     this.input.on('gameobjectover', (p, target) => {
       target.scale = 1.1;
       if (!target.text && !lipAnim[target.text]) return;
+      //vowels.play(target.text);
+      this.sound.play('vowels', vowels.markers[target.text]);
       lips.play(target.text);
     });
 
